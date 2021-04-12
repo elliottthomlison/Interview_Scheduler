6823eceb14ab0b188b775b02aeb87cacf8856456
Elliott Thomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-11 23:42:01 -0700

    Place the days array within the file to provide mock data

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
+import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
 import Button from "components/Button";
 
 storiesOf("Button", module)
@@ -33,12 +35,32 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
+  
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
-  export default function DayListItem(props) {
-    return (
-      <li onClick={() => props.setDay(props.name)}>
-        <h2 className="text--regular">{props.name}</h2>
-        <h3 className="text--light">{props.spots}</h3>
-      </li>
-    );
-  }
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));

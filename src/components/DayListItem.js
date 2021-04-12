597efea307e84efc0b7addf3b474e58246a79ace
Elliott Thomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-11 21:10:48 -0700

    Adjust the code and added further functionality and specificity

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,28 @@
 import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  const formatSpots = function() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    return props.spots + " spots remaining"; // spots stored here. value should change when appt. created or deleted
+  };
+
   return (
-    //li represents the entire day item
-    //h2 should display day name
-    //h3 should display the spots remaining for a day
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li data-testid="day" className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }

60e592b13da62881d5489370a4993e68a6eabc6d
Elliott Thomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-13 18:42:36 -0700

    Create changes that were made

--- stories/index.js
+++ stories/index.js
@@ -103,8 +103,10 @@ storiesOf("DayList", module)
     ));
 
   storiesOf("Appointment", module)
-  .addParameters({
-    backgrounds: [{ name: "white", value: "#fff", default: true }]
-  })
-  .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />)
+    .add("Header", () => <Header time="12pm" />)
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
